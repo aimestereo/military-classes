9afcd100e2506d9cdd99c7686b42ad44d6d789c3
student <student@WS102.(none)>
2013-04-03 15:18:08 +0400

    БД - все таблицы созданы и заполненны тестовыми данными

--- bd/bd.bash
+++ bd/bd.bash
@@ -1,51 +1,81 @@
 #!/bin/bash
 
 MYSQL=`which mysql`
+password="root"
 
 SQL=()
 SQL+="DROP DATABASE IF EXISTS StreetsSituations;"
 SQL+="CREATE DATABASE IF NOT EXISTS StreetsSituations;"
 SQL+="USE StreetsSituations;"
 
-# People
-SQL+="CREATE TABLE people (
+#############################
+# Persons
+#############################
+
+SQL+="CREATE TABLE Persons (
     id   INT,
     firstname VARCHAR(15),
     lastname VARCHAR(15),
     middlename VARCHAR(15)
 );"
 
-SQL+="INSERT INTO people (id,lastname,firstname,middlename) VALUES(0,'Кладов', 'Стас', 'Александрович');"
-SQL+="INSERT INTO people (id,lastname,firstname,middlename) VALUES(1,'Меньшиков', 'Никита', 'Махнович');"
-SQL+="INSERT INTO people (id,lastname,firstname,middlename) VALUES(2,'Прокопчук', 'Андрей', 'Геннадьевич');"
- 
-# Type
-SQL+="CREATE TABLE type (
+SQL+="INSERT INTO Persons (id,lastname,firstname,middlename) VALUES(0,'Кладов', 'Стас', 'Александрович');"
+SQL+="INSERT INTO Persons (id,lastname,firstname,middlename) VALUES(1,'Меньшиков', 'Никита', 'Махнович');"
+SQL+="INSERT INTO Persons (id,lastname,firstname,middlename) VALUES(2,'Прокопчук', 'Андрей', 'Геннадьевич');"
+
+#############################
+# Types
+#############################
+
+SQL+="CREATE TABLE Types (
     id   INT,
-    type VARCHAR(15)
+    name VARCHAR(15)
 );"
 
-SQL+="INSERT INTO type (id,type) VALUES(0,'Авария');"
-SQL+="INSERT INTO type (id,type) VALUES(1,'Столкновение');"
-SQL+="INSERT INTO type (id,type) VALUES(2,'Наезд');"
+SQL+="INSERT INTO Types (id,name) VALUES(0,'Авария');"
+SQL+="INSERT INTO Types (id,name) VALUES(1,'Столкновение');"
+SQL+="INSERT INTO Types (id,name) VALUES(2,'Наезд');"
 
-# Event
-SQL+="CREATE TABLE event (
+#############################
+# ParticipantTypes
+#############################
+
+SQL+="CREATE TABLE ParticipantTypes (
+    id INT,
+    name VARCHAR(15)
+);"
+
+SQL+="INSERT INTO ParticipantTypes (id,name) VALUES(0,'Участник');"
+SQL+="INSERT INTO ParticipantTypes (id,name) VALUES(1,'Виновный');"
+
+#############################
+# Events
+#############################
+
+SQL+="CREATE TABLE Events (
     id   INT,
     typeId INT,
     linkId INT,
     description VARCHAR(100)
 );"
 
-SQL+="INSERT INTO event (id,typeId,linkId,description) VALUES(0,0,0, 'Водитель врезался в столб');"
+SQL+="INSERT INTO Events (id,typeId,linkId,description) VALUES(0,0,0, 'Водитель врезался в столб на 3км Киевского шоссе');"
+SQL+="INSERT INTO Events (id,typeId,linkId,description) VALUES(1,2,1, 'Столкновение на перекрестке Большой и Малой почтовых улиц');"
 
-# link
-SQL+="CREATE TABLE event (
-    linkId INT,
-    peopleId INT
+#############################
+# Links
+#############################
+
+SQL+="CREATE TABLE Links (
+    id INT,
+    personId INT,
+    paticipantTypeId INT,
+    description VARCHAR(100)
 );"
 
-SQL+="INSERT INTO event (id,typeId,linkId,description) VALUES(0,0,0, 'Водитель врезался в столб');"
+SQL+="INSERT INTO Links (id,personId,paticipantTypeId,description) VALUES(0,0,1,'Водитель врезался в столб');"
+SQL+="INSERT INTO Links (id,personId,paticipantTypeId,description) VALUES(1,1,1,'Водитель двигался по Малой почтовой и проехал на красный сигнал светофора, в следствии чего врезался в автомобиль едущий по пересекающей дороге');"
+SQL+="INSERT INTO Links (id,personId,paticipantTypeId,description) VALUES(1,2,0,'Водитель начал движение на зеленый свет и не успел среагировать на нарушителя, гнавшего на красный свет с примыкаемой дороги');"
 
 
 #if [ $# -ne $EXPECTED_ARGS ]
@@ -54,4 +84,25 @@ SQL+="INSERT INTO event (id,typeId,linkId,description) VALUES(0,0,0, 'Водит
 #  exit $E_BADARGS
 #fi
  
-$MYSQL -u root -h 192.168.222.165 -p -e "$SQL"
+$MYSQL -u root -h 192.168.222.165 -p"$password" -e "$SQL"
+
+eventsQuery="
+	SELECT Events.id, Types.name
+	FROM Events 
+	JOIN Types ON typeId = Types.id;"
+
+result=`"$MYSQL" -u root -h 192.168.222.165 -p"$password" -e "$eventsQuery" StreetsSituations`
+
+
+while read -a row
+do
+	echo "..${row[0]}..${row[1]}.."
+done
+
+
+#temp = "
+#	SELECT Events.id, Types.name, Persons.firstname, Links.description 
+#	from Events 
+#	join Types on typeId = Types.id 
+#	join Links on linkId = Links.id 
+#	join Persons on Links.personId = Persons.id;"
